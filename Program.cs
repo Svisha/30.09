54f87a09ec7facc8a8c3363f9e33806c51cde79b
Svisha <Kafraro@gmail.com>
2023-10-28 11:37:38 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Encodings.Web;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+bool a =true;
+while (a==true){
+Console.WriteLine($"Строка номер____");
+}
+
+    }   
+        
+}
